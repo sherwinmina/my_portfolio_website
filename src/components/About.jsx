ab2656764f29dfaee82a056870361600557b76a6
Sherwin Mina <sherwinmina@yahoo.com>
2017-04-04 10:15:56 -0700

    updated css

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,12 +4,18 @@ import { Component } from 'react';
 export default class About extends Component {
   render() {
      return (
-       <div className="container-fluid" style={{paddingTop: '40px'}}>
+       <div className="container-fluid" style={divStyle}>
          <div className ="row">
-         <h1 className="col-md-4">About Me</h1>
-           <p className="col-md-8">  Hello Internet my name is Sherwin Mina I am a developer. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam unde tempora laborum aspernatur praesentium? Odio, sequi maxime, tenetur, natus magnam assumenda labore repellat sunt dolorem vero nesciunt eos illo molestiae. </p> 
+         <h1 className="col-md-4" style={{fontFamily: 'Megrim'}}>About Me</h1>
+           <p className="col-md-8" style={{fontFamily: 'Poiret One'}}>  Hello Internet my name is Sherwin Mina I am a developer. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam unde tempora laborum aspernatur praesentium? Odio, sequi maxime, tenetur, natus magnam assumenda labore repellat sunt dolorem vero nesciunt eos illo molestiae. </p> 
         </div>
        </div>
     );
   }
+}
+
+const divStyle = {
+  fontFamily: 'Grupo',
+  backgroundColor: 'grey',
+  paddingTop: '40px'
 }
