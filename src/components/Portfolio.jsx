fc97f0f9205ca7d12337d7358305ff21d94a8802
Sherwin Mina <sherwinmina@yahoo.com>
2017-04-03 21:29:46 -0700

    added contacts

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -5,7 +5,14 @@ export default class Portfolio extends Component {
   render() {
      return (
        <div>
-            NavBAr
+           <div className="row">
+             <div className="col-md-4"> 
+               <h1>Portfolio</h1>
+            </div>
+            <div className="col-md-8"> 
+               <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid commodi ipsam tempora magni eligendi. Illum quasi tenetur quisquam adipisci sed veritatis corrupti illo laborum ipsa, quod tempore possimus placeat? Soluta.</p>
+            </div>
+           </div>
        </div>
     );
   }
