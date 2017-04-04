be08ab568fde6b816f3308112b9766c1cc38d790
Sherwin Mina <sherwinmina@yahoo.com>
2017-04-03 22:51:04 -0700

    added fonts

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,7 +6,7 @@ export default class About extends Component {
      return (
        <div className="container-fluid" style={{paddingTop: '40px'}}>
          <div className ="row">
-         <h1 className="col-md-4" >About Me</h1>
+         <h1 className="col-md-4">About Me</h1>
            <p className="col-md-8">  Hello Internet my name is Sherwin Mina I am a developer. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam unde tempora laborum aspernatur praesentium? Odio, sequi maxime, tenetur, natus magnam assumenda labore repellat sunt dolorem vero nesciunt eos illo molestiae. </p> 
         </div>
        </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,29 +5,30 @@ export default class Header extends Component {
   render() {
      return (
        <div>
-          <nav className="navbar navbar-toggleable-md navbar-inverse bg-inverse">
+          <nav className="navbar navbar-toggleable-md navbar-inverse bg-inverse" >
             <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
             </button>
-            <a className="navbar-brand" href="#">Sherwin Mina</a>
+            <a className="navbar-brand" href="#" style={divstyles}>Sherwin Mina</a>
             <div className="collapse navbar-collapse" id="navbarText">
               <ul className="navbar-nav mr-auto">
                 <li className="nav-item active">
                   <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="#">Features</a>
+                  <a className="nav-link" href="#">About</a>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="#">Pricing</a>
+                  <a className="nav-link" href="#">Portfolio</a>
                 </li>
               </ul>
-              <span className="navbar-text">
-                Navbar text with an inline element
-              </span>
             </div>
           </nav>
        </div>
     );
   }
+}
+
+const divstyles = {
+   fontFamily: 'Gruppo'
 }
