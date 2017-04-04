35b0c88a99cbc5767e45b29bdad6ecd10ae10229
Sherwin Mina <sherwinmina@yahoo.com>
2017-04-03 17:43:44 -0700

    fixed component wire up

--- src/index.jsx
+++ src/index.jsx
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 
 import App from './components/app';
+import reducers from './reducers';
 
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
