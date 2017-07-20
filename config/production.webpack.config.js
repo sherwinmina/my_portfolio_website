360dbba9c8f28577c092927ae1ac21901910e035
Sherwin Mina <sherwinmina@yahoo.com>
2017-07-20 00:43:14 -0700

    got most of the producttion set up running

--- config/production.webpack.config.js
+++ config/production.webpack.config.js
@@ -20,7 +20,7 @@ module.exports = function(env) {
     rules: [
       {
       exclude: /node_modules/,
-      loader: 'babel',
+      loader: 'babel-loader',
       query: {
       presets: ['react', 'es2015', 'stage-1']
       },
